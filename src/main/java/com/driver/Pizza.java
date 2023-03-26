4e19418c0508f348f0a427c2e1f228e395851c5e
vaishali-sharma <iitmgwl1975@gmail.com>
2023-03-26 19:54:52 +0530

    [task completed]

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -9,6 +9,13 @@ public class Pizza {
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        if(isVeg==true)
+        { price=300;
+        System.out.println("Base Price Of The Pizza:"+ price);}
+        else
+        {price=400;
+            System.out.println("Base Price Of The Pizza:"+ price);}
+
     }
 
     public int getPrice(){
@@ -16,19 +23,30 @@ public class Pizza {
     }
 
     public void addExtraCheese(){
-        // your code goes here
+        price+=80;
+        System.out.println("Extra Cheese Added: 80");
     }
 
     public void addExtraToppings(){
         // your code goes here
+        if(isVeg==true)
+        {price+=70;
+        System.out.println("Extra Toppings Added: 70");}
+        else
+        {
+            {price+=120;
+                System.out.println("Extra Toppings Added: 120");}
+        }
     }
 
     public void addTakeaway(){
         // your code goes here
+        price+=20;
+        System.out.println("Paperbag Added: 20");
     }
 
-    public String getBill(){
-        // your code goes here
+    public String getBill()
+    {   bill+="Total Price:"+price;
         return this.bill;
     }
 }
